d4f808980e27be6b066ad890158035c2069c36a9
Creeper1212 <148118420+Creeper1212@users.noreply.github.com>
2026-03-05 12:31:43 +0100

    Refactor ShaderSubProgram for version handling

--- USCSandbox/Processor/ShaderSubProgram.cs
+++ USCSandbox/Processor/ShaderSubProgram.cs
@@ -1,4 +1,4 @@
-﻿using AssetRipper.Primitives;
+using AssetRipper.Primitives;
 using AssetsTools.NET;
 
 namespace USCSandbox.Processor
@@ -11,58 +11,78 @@ namespace USCSandbox.Processor
         public int StatsFlow;
         public int StatsTempRegister;
 
-        public List<string> GlobalKeywords;
-        public List<string> LocalKeywords;
+        public List<string> GlobalKeywords = new List<string>();
+        public List<string> LocalKeywords = new List<string>();
 
         public byte[] ProgramData;
         public ParserBindChannels BindChannels;
 
         public ShaderParams ShaderParams;
 
+        // Version constants based on reference AssetRipper logic
+        private static bool HasStatsTempRegister(UnityVersion version) => version.IsGreaterEqual(5, 5);
+        private static bool HasLocalKeywords(UnityVersion version) => version.IsGreaterEqual(2019);
+        private static bool HasMergedKeywords(UnityVersion version) => version.IsGreaterEqual(2021, 2);
+
         public ShaderSubProgram(AssetsFileReader r, UnityVersion version)
         {
-            var hasStatsTempRegister = version.IsGreaterEqual(5, 5);
-            var hasLocalKeywords = version.IsLess(2021, 2) && version.IsGreaterEqual(2019, 1);
-
+            // Unity writes a version/date header at the start of every subprogram block
             var blobVersion = r.ReadInt32();
+
             ProgramType = r.ReadInt32();
             StatsALU = r.ReadInt32();
             StatsTEX = r.ReadInt32();
             StatsFlow = r.ReadInt32();
-            if (hasStatsTempRegister)
+
+            if (HasStatsTempRegister(version))
             {
                 StatsTempRegister = r.ReadInt32();
             }
 
-            var globalKeywordCount = r.ReadInt32();
-            GlobalKeywords = new List<string>(globalKeywordCount);
-            for (var i = 0; i < globalKeywordCount; i++)
+            if (HasMergedKeywords(version))
             {
-                GlobalKeywords.Add(r.ReadCountStringInt32());
-                r.Align();
-            }
-            if (hasLocalKeywords)
-            {
-                var localKeywordCount = r.ReadInt32();
-                LocalKeywords = new List<string>(localKeywordCount);
-                for (var i = 0; i < localKeywordCount; i++)
+                // In 2021.2+, Global and Local keywords are one single array
+                int keywordCount = r.ReadInt32();
+                for (int i = 0; i < keywordCount; i++)
                 {
-                    LocalKeywords.Add(r.ReadCountStringInt32());
+                    GlobalKeywords.Add(r.ReadCountStringInt32());
                     r.Align();
                 }
             }
             else
             {
-                LocalKeywords = new List<string>(0);
+                // Older versions: Global keywords first
+                int globalKeywordCount = r.ReadInt32();
+                for (int i = 0; i < globalKeywordCount; i++)
+                {
+                    GlobalKeywords.Add(r.ReadCountStringInt32());
+                    r.Align();
+                }
+
+                // Then Local keywords (introduced in 2019.1)
+                if (HasLocalKeywords(version))
+                {
+                    int localKeywordCount = r.ReadInt32();
+                    for (int i = 0; i < localKeywordCount; i++)
+                    {
+                        LocalKeywords.Add(r.ReadCountStringInt32());
+                        r.Align();
+                    }
+                }
             }
 
+            // Read the actual shader bytecode
             var programDataSize = r.ReadInt32();
             ProgramData = r.ReadBytes(programDataSize);
             r.Align();
 
+            // Read hardware binding channels
             BindChannels = new ParserBindChannels(r);
 
-            if (version.IsLess(2021))
+            // Read parameters (Constant Buffers, Textures, Samplers)
+            // Note: In very new Unity versions (2021+), this might be in a separate ParameterBlob,
+            // but we check if there is data remaining in this subprogram block.
+            if (r.BaseStream.Position < r.BaseStream.Length)
             {
                 ShaderParams = new ShaderParams(r, version, false);
             }
